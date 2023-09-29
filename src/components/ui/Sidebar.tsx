ff9e43c1deba7922cc6dea8dbb4fb315b9954913
EmranSWE <mdemran.swe@gmail.com>
2023-09-29 19:02:01 +0600

    [DONE] role based user implementation

--- src/components/ui/Sidebar.tsx
+++ src/components/ui/Sidebar.tsx
@@ -1,91 +1,52 @@
 "use client";
-import React, { useState } from "react";
 
-import { Layout, Menu, theme } from "antd";
-import type { MenuProps } from "antd/es/menu";
-const { Header, Content, Footer, Sider } = Layout;
+import { useState } from "react";
+import { Layout, Menu } from "antd";
 
-type MenuItem = Required<MenuProps>["items"][number];
-import {
-  DesktopOutlined,
-  FileOutlined,
-  PieChartOutlined,
-  TeamOutlined,
-  UserOutlined,
-} from "@ant-design/icons";
-import { USER_ROLE } from "@/constants/role";
 import { sidebarItems } from "@/constants/sidebarItems";
-function getItem(
-  label: React.ReactNode,
-  key: React.Key,
-  icon?: React.ReactNode,
-  children?: MenuItem[]
-): MenuItem {
-  return {
-    key,
-    icon,
-    children,
-    label,
-  } as MenuItem;
-}
+import { USER_ROLE } from "@/constants/role";
 
-const items: MenuItem[] = [
-  getItem("Option 1", "1", <PieChartOutlined />),
-  getItem("Option 2", "2", <DesktopOutlined />),
-  getItem("User", "sub1", <UserOutlined />, [
-    getItem("Tom", "3"),
-    getItem("Bill", "4"),
-    getItem("Alex", "5"),
-  ]),
-  getItem("Team", "sub2", <TeamOutlined />, [
-    getItem("Team 1", "6"),
-    getItem("Team 2", "8"),
-  ]),
-  getItem("Files", "9", <FileOutlined />),
-];
+const { Sider } = Layout;
 
-const Sidebar = () => {
+const SideBar = () => {
   const [collapsed, setCollapsed] = useState(false);
-  const role = USER_ROLE.ADMIN;
-  const {
-    token: { colorBgContainer },
-  } = theme.useToken();
+
+  const role = USER_ROLE.STUDENT;
+
   return (
-    <div>
-      <Sider
-        collapsible
-        collapsed={collapsed}
-        onCollapse={(value) => setCollapsed(value)}
-        width={280}
+    <Sider
+      collapsible
+      collapsed={collapsed}
+      onCollapse={(value) => setCollapsed(value)}
+      width={280}
+      style={{
+        overflow: "auto",
+        height: "100vh",
+        position: "sticky",
+        left: 0,
+        top: 0,
+        bottom: 0,
+      }}
+    >
+      <div
         style={{
-          overflow: "auto",
-          height: "100vh",
-          position: "sticky",
-          left: 0,
-          top: 0,
-          bottom: 0,
+          color: "white",
+          fontSize: "2rem",
+          textAlign: "center",
+          fontWeight: "bold",
+          marginBottom: "1rem",
         }}
       >
-        <div
-          style={{
-            color: "white",
-            fontSize: "2rem",
-            textAlign: "center",
-            fontWeight: "bold",
-            marginBottom: "1rem",
-          }}
-        >
-          PH-University
-        </div>
-        <Menu
-          theme="dark"
-          defaultSelectedKeys={["1"]}
-          mode="inline"
-          items={sidebarItems(role)}
-        />
-      </Sider>
-    </div>
+        PH-University
+      </div>
+      <Menu
+        theme="dark"
+        defaultSelectedKeys={["1"]}
+        mode="inline"
+        items={sidebarItems(role)}
+      />
+    </Sider>
   );
 };
 
-export default Sidebar;
+export default SideBar;
